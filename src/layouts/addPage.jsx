e367909188705b4221faed326f66915819a8e091
Dmitry <dmitry27zh@gmail.com>
2023-09-02 16:38:01 +0300

    Create AddPage

---
+++ src/layouts/addPage.jsx
@@ -0,0 +1,12 @@
+import Edit from '../components/edit'
+import Page from './page'
+
+const AddPage = () => {
+  return (
+    <Page title="Add new article">
+      <Edit />
+    </Page>
+  )
+}
+
+export default AddPage
