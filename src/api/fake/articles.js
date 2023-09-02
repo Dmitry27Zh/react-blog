32a6cecb1436974ca5215c49bea973ab00809b43
Dmitry <dmitry27zh@gmail.com>
2023-09-02 16:10:55 +0300

    Add articles fetchAll fake api

---
+++ src/api/fake/articles.js
@@ -0,0 +1,26 @@
+const articles = [
+  {
+    id: '1',
+    name: 'Article 1',
+    short: 'Short 1',
+    long: 'Long 1 asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg',
+  },
+  {
+    id: '2',
+    name: 'Article 2',
+    short: 'Short 2',
+    long: 'Long 2 asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg',
+  },
+  {
+    id: '3',
+    name: 'Article 3',
+    short: 'Short 3',
+    long: 'Long 3 asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg asdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfgasdfgasdfgasd fgasdfgasdfgas dfgasdfgasdfg asdfgasdfgasdfgasdfga dfgasdfgasdfg',
+  },
+]
+
+export const fetchAll = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(articles)
+  }, 1000)
+})
---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import * as articles from './fake/articles'
+
+const API = {
+  articles,
+}
+
+export default API
