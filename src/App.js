96a79d7f5547e0c2acd60d40ce677ccd13096f0b
Dmitry <dmitry27zh@gmail.com>
2023-09-02 17:45:54 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/header'
-import ArticlePage from './layouts/articlePage'
 import ArticlesPage from './layouts/articlesPage'
+import DashboardPage from './layouts/dashboardPage'
+import AddPage from './layouts/addPage'
+import LoginPage from './layouts/loginPage'
+import SignUpPage from './layouts/signUpPage'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <ArticlesPage />
+        <Routes>
+          <Route path="/articles?" element={<ArticlesPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/add" element={<AddPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+        </Routes>
       </div>
     </div>
   )
