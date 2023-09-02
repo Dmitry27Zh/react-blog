751e4030777028266733696034ada5a11b452d69
Dmitry <dmitry27zh@gmail.com>
2023-09-02 17:45:29 +0300

    Create SignUpPage

---
+++ src/layouts/signUpPage.jsx
@@ -0,0 +1,12 @@
+import PageSpecial from './pageSpecial'
+import SignUp from '../components/signUp'
+
+const SignUpPage = () => {
+  return (
+    <PageSpecial title="Sign up">
+      <SignUp />
+    </PageSpecial>
+  )
+}
+
+export default SignUpPage
