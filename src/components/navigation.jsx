b2bbbfafa6f29c09c995adb2f6f9f6649eb4e101
Dmitry <dmitry27zh@gmail.com>
2023-09-02 17:47:34 +0300

    Add Link in Navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,20 +1,22 @@
+import { Link } from 'react-router-dom'
+
 const Navigation = () => {
   return (
     <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
       <li>
-        <a href="/" className="nav-link px-2 link-secondary">
+        <Link to="/" className="nav-link px-2 link-secondary">
           Articles
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="/" className="nav-link px-2">
+        <Link to="/dashboard" className="nav-link px-2">
           Dashboard
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="/" className="nav-link px-2">
+        <Link to="/add" className="nav-link px-2">
           Add
-        </a>
+        </Link>
       </li>
     </ul>
   )
