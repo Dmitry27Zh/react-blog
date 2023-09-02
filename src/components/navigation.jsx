199797707a2474defde1d00431c24d6144a869bb
Dmitry <dmitry27zh@gmail.com>
2023-09-02 19:45:06 +0300

    Fix NavLink active state in nested route

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
       <li>
-        <NavigationLink to="/">Articles</NavigationLink>
+        <NavigationLink to="/articles">Articles</NavigationLink>
       </li>
       <li>
         <NavigationLink to="/dashboard">Dashboard</NavigationLink>
