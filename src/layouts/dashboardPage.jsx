6fc61bfa9030640e3ad64f1f2c7314336d8bd8b8
Dmitry <dmitry27zh@gmail.com>
2023-09-02 16:32:36 +0300

    Create DashboardPage

---
+++ src/layouts/dashboardPage.jsx
@@ -0,0 +1,12 @@
+import Page from './page'
+import Dashboard from '../components/dashboard'
+
+const DashboardPage = () => {
+  return (
+    <Page title="Articles">
+      <Dashboard />
+    </Page>
+  )
+}
+
+export default DashboardPage
