f2e8b12938f98991dbec7e2e30d24933c6a5d82c
Dmitry <dmitry27zh@gmail.com>
2023-09-03 12:14:25 +0300

    Add content in Edit

--- src/components/edit.jsx
+++ src/components/edit.jsx
@@ -1,5 +1,31 @@
 const Edit = () => {
-  return <div>Add</div>
+  return (
+    <form>
+      <div className="row g-3">
+        <div className="col-sm-6 me-5">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-control" type="text" id="name" />
+        </div>
+        <div className="col-sm-6 me-5">
+          <label className="form-label" htmlFor="short">
+            Short text
+          </label>
+          <textarea className="form-control" type="text" id="short" style={{ height: '4rem' }} />
+        </div>
+        <div className="col-12 me-5">
+          <label className="form-label" htmlFor="long">
+            Long text
+          </label>
+          <textarea className="form-control" type="text" id="long" style={{ height: '10rem' }} />
+        </div>
+      </div>
+      <button className="btn btn-outline-dark mt-4" type="submit">
+        Submit
+      </button>
+    </form>
+  )
 }
 
 export default Edit
